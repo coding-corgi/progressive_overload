870e83ff5aa880005c65fb5071fd937c5c5026c0
codingcorgi <tbvjsks21@naver.com>
2025-06-22 19:24:07 +0900

    feat: Set up local database environment with Docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        url: configService.get<string>('DATABASE_URL'),
+        synchronize: true,
+        entities: [__dirname + `/../**/*.entity.js`],
+      }),
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
